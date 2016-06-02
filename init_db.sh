f7946ff0acb31289252fa2602471019a8a3d8d54
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-06-02 14:58:19 +0200

    Reorganization of the data for the import.

--- init_db.sh
+++ init_db.sh
@@ -10,11 +10,11 @@ python3.5 manager.py import_page Glossary data/pages/glossary.html en
 python3.5 manager.py import_page About data/pages/about.html en
 
 # Initializes the structure of the shelters
-python3.5 manager.py init_db_structure data/Shelters_Structure.csv
+python3.5 manager.py init_db_structure data/shelters/Shelters_Structure.csv
 
 # Initializes the administrator (owner of the imported shelters)
 python3.5 manager.py create_admin_user
 
 # Import of the shelters from the CSV files
-python3.5 manager.py import_shelters admin data/20150518_Haiti_shelters.csv
-python3.5 manager.py import_shelters admin data/Phil-Bangla-Burundi.csv
+python3.5 manager.py import_shelters admin data/shelters/20150518_Haiti_shelters.csv
+python3.5 manager.py import_shelters admin data/shelters/Phil-Bangla-Burundi.csv
