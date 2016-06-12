f1f8d0ca9f11eeceb006a0db0b6de92d17dd7ca7
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-06-12 11:36:56 +0200

    Displays a message when a shelter has been created.

--- web/processors.py
+++ web/processors.py
@@ -13,16 +13,16 @@
 __author__ = "Cedric Bonhomme"
 __version__ = "$Revision: 0.1 $"
 __date__ = "$Date: 2016/03/30$"
-__revision__ = "$Date: 2016/06/06 $"
+__revision__ = "$Date: 2016/06/12 $"
 __copyright__ = "Copyright (c) "
 __license__ = ""
 
-from flask import request
+from flask import request, flash
 from flask_login import current_user
 from flask_restless import ProcessingException
 
 from web.views.common import login_user_bundle
-from web.models import User, Property, Attribute
+from web.models import User
 
 def auth_func(*args, **kw):
     if request.authorization:
@@ -41,3 +41,10 @@ def auth_func(*args, **kw):
 
 def shelter_POST_preprocessor(data=None, **kw):
     data["user_id"] = current_user.id
+    if not current_user.is_admin:
+        data["is_active"] = True
+        flash("Your shelter has been created.", 'success')
+    else:
+        flash("Thank you! Your shelter has been created but is not yet public." +
+            " An administrator will review it. You can already edit it.",
+            'success')
