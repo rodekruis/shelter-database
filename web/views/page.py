d0dc612cdcdc28b2842e319d68299d01c437840a
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-06-13 08:43:14 +0200

    translations improvements.

--- web/views/page.py
+++ web/views/page.py
@@ -18,15 +18,18 @@ __copyright__ = "Copyright 2016 Luxembourg Institute of Science and Technology"
 __license__ = ""
 
 from flask import render_template, current_app
+from flask_babel import get_locale
 
 from web.models import Page
 
 @current_app.route('/page/<string:page_name>', methods=['GET'])
 def recommendations(page_name):
-    language_code = "en"
+    language_code = get_locale().language
     page = Page.query.filter(Page.name==page_name,
                             Page.language_code==language_code).first()
-    if page:
-        return render_template('help_pages.html', page=page)
-    else:
-        return render_template('errors/404.html'), 404
+    if not page:
+        page = Page.query.filter(Page.name==page_name,
+                                Page.language_code=='en').first()
+        if not page:
+            return render_template('errors/404.html'), 404
+    return render_template('help_pages.html', page=page)
