0a3431a597cb0ff000431dcc79ceeeec3048a3c1
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-06-17 09:02:48 +0200

    Add alembic script to fix the type of the column updated_at of shelters

---
+++ migrations/versions/953c5e0cebad_fix_the_type_of_the_updated_at_column_.py
@@ -0,0 +1,28 @@
+"""fix the type of the updated_at column for shelters
+
+Revision ID: 953c5e0cebad
+Revises: 9bfedc780ac5
+Create Date: 2016-06-17 09:00:35.875583
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '953c5e0cebad'
+down_revision = '9bfedc780ac5'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.drop_column('shelter', 'updated_at')
+    op.add_column('shelter', sa.Column('updated_at', sa.DateTime(),
+                    default=datetime.now))
+
+
+def downgrade():
+    op.drop_column('shelter', 'updated_at')
+    op.add_column('shelter', sa.Column('updated_at', sa.Boolean(),
+                    default=datetime.now))
