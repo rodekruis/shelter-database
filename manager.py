041598cbe4ad865fcf190fb364fd8a17cd15ebb6
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-06-17 08:55:24 +0200

    Added migrations scripts

--- manager.py
+++ manager.py
@@ -19,11 +19,19 @@ __license__ = ""
 
 from bootstrap import app, db, populate_g
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 import scripts
 import web.models
 
 manager = Manager(app)
+
+
+Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
 
 @manager.command
 def uml_graph():
