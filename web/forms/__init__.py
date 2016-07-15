3e18dbd50065e087e0e451b7f6542c1281af2439
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-07-15 13:26:13 +0200

    Refactorization of the root of the project.

--- web/forms/__init__.py
+++
@@ -1,5 +0,0 @@
-from .login import LoginForm, SignupForm
-from .profile import ProfileForm
-from .user_form import CreateUserForm, EditUserForm
-from .category import CategoryForm
-from .attribute import AttributeForm
