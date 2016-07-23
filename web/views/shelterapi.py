c8e163e3b782bc9002118dd4a8ee68a9df480e8c
Aron Gergely <aaron.gergely@yahoo.com>
2016-07-23 20:50:52 +0200

    Added function:  get all available values for an attribute

--- web/views/shelterapi.py
+++ web/views/shelterapi.py
@@ -22,35 +22,67 @@ api_bp = Blueprint('api for shelter', __name__, url_prefix='/api/v0.1')
 
 def tree():
 	return defaultdict(tree)
-	
+
+@api_bp.route('/', methods=['GET'])
+def apimessage():
+    message = tree()
+    message["API version"] = 0.1
+    message["Message"] = "This is the development API"
+    return jsonify(message)
+
+@api_bp.route('/attributes/<attribute_name>', methods=['GET'])
+def getattributes(attribute_name):
+    """Returns available values for a given attribute name, separated by semicolons"""
+    result= tree()
+    
+    attributes = Attribute.query.filter(Attribute.name==attribute_name).\
+                                first().associated_values
+    
+    for attribute in attributes:
+    	if result[attribute_name]:
+    		result[attribute_name] += (";" + attribute.name)
+    	else:
+    		result[attribute_name] = attribute.name
+    		
+    return jsonify(result)
 
 @api_bp.route('/shelters', methods=['GET'])
 def allshelters():
-    """Return all shelters and their properties """
+    """Returns all shelters and their properties"""
     
     result = tree()
     shelter_properties  = Property.query.all()
-    attribute_table = Attribute.query.all()
+    attributes = Attribute.query.all()
     
-    for item in shelter_properties:
-    	for y in attribute_table:
-    		if y.id == item.attribute_id:
-    			result[item.shelter_id][y.name] = item.values[0].name
+    for shelter_property in shelter_properties:
+    	for attribute in attributes:
+    		if attribute.id == shelter_property.attribute_id:
+    			result[shelter_property.shelter_id][attribute.name] = shelter_property.values[0].name
 
     return jsonify(result)
 
 @api_bp.route('/shelters/<int:shelter_id>', methods=['GET'])
-def ashelters(shelter_id):
-    """Return specific shelter with its properties"""
+def shelters(shelter_id):
+    """Returns specific shelter with its properties"""
     result = tree()
     shelter_properties  = Property.query.filter(Property.shelter_id==shelter_id)
-    attribute_table = Attribute.query.all()
-    for item in shelter_properties:
-    	print("ping")
-    	for y in attribute_table:
-    		if y.id == item.attribute_id:
-    			result[item.shelter_id][y.name] = item.values[0].name
+    attributes = Attribute.query.all()
+    for shelter_property in shelter_properties:
+    	for attribute in attributes:
+    		if attribute.id == shelter_property.attribute_id:
+    			result[shelter_property.shelter_id][attribute.name] = shelter_property.values[0].name
     
     return jsonify(result)
+
+
+#@api_bp.route('/shelters/<attribute_name>/<attribute_value>', methods=['GET'])
+#def attributes(attribute_name, attribute_value):
+#    """Returns all shelters which match a specific attribute name + value"""
+#    shelter_properties = Property.query.filter(Property.attribute.has(name=attribute_name))
+#    attributes = Attribute.query.filter(Attribute.values.has(value=attribute_value))
+#    
+#    return jsonify(result)
+
+
 
 
