458152760b1de700b06673461104ff0eddfd7fe4
Krystyna Milian <krystyna.milian@gmail.com>
2016-07-29 20:07:06 +0200

    Improved layout, fixed leaflet buttons, added topography chart, prepared additional advenced filters

--- src/web/static/js/dashboard.js
+++ src/web/static/js/dashboard.js
@@ -12,15 +12,16 @@
             var countryChart = dc.rowChart('#chart-bar-country')
             var mapChart = dc_leaflet.markerChart("#chart-map")
             var tableChart = dc.dataTable("#shelters-table")
+            var topographyChart = dc.rowChart('#chart-topography');
 
 
             d3.csv('/static/data/shelters-sample.csv', function (data) {
-            // d3.json("/api/v0.1/shelters", function(dataObject) {
-            //
-            //     var data = []
-            //     for (var key in dataObject) {
-            //        data.push(dataObject[key])
-            //     }
+            //d3.json("api/v0.1/shelters", function(dataObject) {
+
+              //  var data = []
+              //  for (var key in dataObject) {
+              //     data.push(dataObject[key])
+              //  }
                 var dateFormat = d3.time.format('%Y');
 
                 var ndx = crossfilter(data);
@@ -31,14 +32,19 @@
                 var all = ndx.groupAll();
                 var dataCount = dc.dataCount('#data-count')
                 var zoneDimension = ndx.dimension(function (d) {
-                    return d.Zone;
-                });
+                     if (d.zone) {
+                        return d.zone;
+                    } else {
+                        return "No data"
+                    }
+                 });
+
                 var zoneCount = zoneDimension.group().reduceCount()
 
 
                 var crisisDimension = ndx.dimension(function (d) {
-                    if (d['Associated disaster / Immediate cause']) {
-                        return d['Associated disaster / Immediate cause'];
+                    if (d['associateddisasterimmediatecause']) {
+                        return d['associateddisasterimmediatecause'];
                     } else {
                         return 'No data'
                     }
@@ -46,8 +52,8 @@
                 });
                 var crisisCount = crisisDimension.group().reduceCount()
                 var climateDimension = ndx.dimension(function (d) {
-                    if (d['Climate zone']) {
-                        return d['Climate zone'];
+                    if (d['climatezone']) {
+                        return d['climatezone'];
                     } else {
                         return 'No data'
                     }
@@ -57,8 +63,8 @@
 
 
                 var timeDimension = ndx.dimension(function (d) {
-                    if (d['Year of construction (first completed shelters)']) {
-                        return d3.time.year(dateFormat.parse(d['Year of construction (first completed shelters)']));
+                    if (d['yearofconstructionfirstcompletedshelters']) {
+                        return d3.time.year(dateFormat.parse(d['yearofconstructionfirstcompletedshelters']));
                     }
                     else {
                         return undefined;
@@ -67,8 +73,8 @@
                 });
                 var timeCount = timeDimension.group().reduceCount(
                         function (d) {
-                            if (d['Year of construction (first completed shelters)']) {
-                                return d['Year of construction (first completed shelters)'];
+                            if (d['yearofconstructionfirstcompletedshelters']) {
+                                return d['yearofconstructionfirstcompletedshelters'];
                             } else {
                                 return undefined;
                             }
@@ -77,9 +83,9 @@
                 );
 
                 var shelters = ndx.dimension(function (d) {
-                    if (d['GPS Latitude'] && d['GPS Longitude'])
+                    if (d['gpslatitude'] && d['gpslongitude'])
                     {
-                        return [d['GPS Latitude'], d['GPS Longitude']];
+                        return [d['gpslatitude'], d['gpslongitude']];
                     } else {
                         return undefined;
                     }
@@ -87,14 +93,24 @@
                 });
 
                 var countryDimension = ndx.dimension(function (d) {
-                    if (d.Country) {
-                        return d.Country;
+                    if (d.country) {
+                        return d.country;
                     } else {
                         return 'No data'
                     }
 
                 })
                 var countryCount = countryDimension.group().reduceCount()
+
+                var topographyDimension = ndx.dimension(function (d) {
+                    if (d.topography) {
+                        return d.topography;
+                    } else {
+                        return 'No data'
+                    }
+
+                })
+                var topographyCount = topographyDimension.group().reduceCount()
 
 
                 var sheltersGroup = shelters.group().reduceCount();
@@ -142,7 +158,7 @@
                         .dimension(timeDimension)
                         .group(timeCount)
                         .barPadding(5)
-                        .x(d3.time.scale().domain([new Date(2008, 01, 01), new Date()]))
+                        .x(d3.time.scale().domain([new Date(2003, 01, 01), new Date()]))
                         .xUnits(d3.time.year)
                         .on("filtered", getFiltersValues)
                          .yAxis().tickFormat(
@@ -150,14 +166,26 @@
 
 
                 countryChart
-                        .width(150)
-                        .height(250)
+                        .width(200)
+                        .height(200)
                         .margins({left:0, right:10, top:10, bottom:20})
                         .dimension(countryDimension)
                         .group(countryCount)
                         .on("filtered", getFiltersValues)
                         .xAxis().tickFormat(
                         function (v) { return d3.format('f')(v) ; });
+
+                countryChart.xAxis().ticks(10)
+
+                topographyChart
+                        .width(200)
+                        .height(200)
+                        .margins({left:0, right:10, top:10, bottom:20})
+                        .dimension(topographyDimension)
+                        .group(topographyCount)
+                        .on("filtered", getFiltersValues)
+                        .xAxis().tickFormat(
+                         function (v) { return d3.format('f')(v) ; });
 
 
 
@@ -168,22 +196,22 @@
                         })
                         .columns([
                             function (d) {
-                                return d.ID;
+                                return d.id;
                             },
                             function (d) {
-                                return d['Name of shelter'];
+                                return d['nameofshelter'];
                             },
                             function (d) {
-                                return d.Zone;
+                                return d.zone;
                             },
                             function (d) {
-                                return d.Country;
+                                return d.country;
                             },
                             function (d) {
-                                return d['Associated disaster / Immediate cause'];
+                                return d[''];
                             },
                             function (d) {
-                                return d['Climate zone'];
+                                return d['climatezone'];
                             }
                         ])
                         .on('renderlet', function (table) {
@@ -199,46 +227,68 @@
                 // Init chart filters
 
                 function initFilters() {
-                    var parseHash = /^#zone=([A-Za-z0-9,_\-\/\s]*)&crisis=([A-Za-z0-9,_\-\/\s]*)&climate=([A-Za-z0-9,_\-\/\s]*)&time=([A-Za-z0-9,_\-\/\s\(\):+]*)&country=([A-Za-z0-9,_\-\/\s]*)&map=(.*)$/;
+
+                    var parseHash = /^#zone=([A-Za-z0-9,_\-\/\s]*)&crisis=([A-Za-z0-9,_\-\/\s]*)&climate=([A-Za-z0-9,_\-\/\s]*)&time=([A-Za-z0-9,_\-\/\s\(\):+]*)&country=([A-Za-z0-9,_\-\/\s]*)&map=(.*)&topography=([A-Za-z0-9,_\-\/\s]*)$/;
+
                     var parsed = parseHash.exec(decodeURIComponent(location.hash.replace(/\+/g, ' ')));
-                    // console.log("parsed:", parsed)
+                    console.log(location.hash)
+                    console.log("parsed:", parsed)
                     function filter(chart, rank) {
 
                         if (parsed[rank] == "") {
                             chart.filter(null);
                         }
                         else {
-                            var filterValues = parsed[rank].split(",");
+                                var filterValues = parsed[rank].split(",");
 
-                            if (rank ==4 && filterValues.length==2) {   //filtering timeChart
-                                filterValues[i] = new Date(filterValues[i])
-                                var start = new Date(filterValues[0])
-                                var end = new Date(filterValues[1])
-                                chart.filter(dc.filters.RangedFilter(start,end))
-                            }
-                            else {
-                                if (rank ==6) {
-                                    // console.log('parsed:', filterValues)
-                                    // filterValues = JSON.parse(filterValues)
-                                    // console.log(filterValues)
+                                // if (rank ==4 && filterValues.length==2) {   //filtering timeChart
+                                //     filterValues[i] = new Date(filterValues[i])
+                                //     var start = new Date(filterValues[0])
+                                //     var end = new Date(filterValues[1])
+                                //     chart.filter(dc.filters.RangedFilter(start,end))
+                                // }
+                                // else {
+                                //     if (rank ==6) {
+                                //         // console.log('parsed:', filterValues)
+                                //         // filterValues = JSON.parse(filterValues)
+                                //         // console.log(filterValues)
+                                //
+                                //     } else {
+                                //         for (var i = 0; i < filterValues.length; i++) {
+                                //             chart.filter(filterValues[i]);
+                                //         }
+                                //     }
+                                // }
 
-                                } else {
-                                    for (var i = 0; i < filterValues.length; i++) {
-                                        chart.filter(filterValues[i]);
-                                    }
+                                switch(rank) {
+                                    case 4: //timeChart
+                                        chart.filter(null);
+                                        if (filterValues.length==2) {
+                                            filterValues[i] = new Date(filterValues[i])
+                                            var start = new Date(filterValues[0])
+                                            var end = new Date(filterValues[1])
+                                            chart.filter(dc.filters.RangedFilter(start,end))
+                                        }
+                                        break;
+                                    case 6: //mapChart
+                                        // console.log('parsed:', filterValues)
+                                        // filterValues = JSON.parse(filterValues)
+                                        // console.log(filterValues)
+                                        break;
+                                    default:
+                                        for (var i = 0; i < filterValues.length; i++) {
+                                            chart.filter(filterValues[i]);
+                                        }
                                 }
                             }
-
-                        }
-                    }
-
-                    if (parsed) {
                         filter(zoneChart, 1);
                         filter(crisisChart, 2);
                         filter(climateChart, 3);
                         filter(timeChart, 4);
                         filter(countryChart, 5)
-                        filter(mapChart, 6);
+                        // filter(mapChart, 6);
+                        filter(topographyChart,6)
+
                     }
 
                     dc.renderAll();
@@ -271,7 +321,9 @@
                     {name: 'climate', value: climateChart.filters()},
                     {name: 'time', value: timeChart.filters()},
                     {name: 'country', value: countryChart.filters()},
-                    { name: 'map', value: JSON.stringify(mapChart.filters())}
+                    // {name: 'map', value: JSON.stringify(mapChart.filters())},
+                    {name: 'topography', value: topographyChart.filters()}
+
                     ];
 
                 // console.log("map:")
@@ -301,6 +353,17 @@
                 dc.redrawAll();
               });
 
+
+          d3.selectAll('#cost').on('click', function () {
+                costChart.filterAll();
+
+              dc.redrawAll();
+          });
+
+          d3.selectAll('#topography').on('click', function () {
+                topographyChart.filterAll();
+                dc.redrawAll();
+            });
 
         })
 
