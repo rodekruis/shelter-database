0ef04b8bf6ac34c21510013c35cee7486b38080a
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-09-08 07:59:34 +0200

    Updated comment.

--- create_db.sh
+++ create_db.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 
-# drop completely the db (with the sequences)
+# drop completely the db (with the triggers, sequences, etc.)
 sudo -u postgres dropdb shelter
 sudo -u postgres createdb shelter --no-password
 echo "ALTER USER pgsqluser WITH ENCRYPTED PASSWORD 'pgsqlpwd';" | sudo -u postgres psql
