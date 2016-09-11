8023cf7a85c09e0ec7b3909f758b07915b6c8d69
Maarten van der Veen <mvanderveen@redcross.nl>
2016-09-11 10:09:28 +0200

    fix libs and add legend

--- src/web/static/js/dashboard.js
+++ src/web/static/js/dashboard.js
@@ -529,6 +529,20 @@ function addLayersToChart(mapChart) {
     mapChart._doRender()
     var map = mapChart.map()
 
+	// add legend control
+	var Legend =  new L.Control.Legend({
+			position: 'topleft',
+			collapsed: true,
+			controlButton: {
+				title: "Legend"
+			}
+	});
+			
+	map.addControl( Legend );
+
+	$(".legend-container").append( $("#legend") );
+	$(".legend-toggle").append( "<i class='legend-toggle-icon fa fa-info fa-2x' style='color: #000'></i>" );
+
     map.addLayer(redCrossLayer)
     L.control.layers(null, overlayMaps).addTo(map);
 
