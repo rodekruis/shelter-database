bbee93a2664fcda390abf643f750401a64c89f58
= <mvanderveen@redcross.nl>
2016-10-05 22:47:51 +0200

    fix spinner on index

--- src/web/static/js/index.js
+++ src/web/static/js/index.js
@@ -29,6 +29,47 @@ if("/" === loc) {
 		})
 	}
 	
+	// function to generate blocks
+	var generate = function generate(flexbox, data) {
+
+		var fb = d3.select(flexbox);
+
+		// create a box for each object in the data
+		var box = fb.selectAll('div')
+		  .data(data)
+		  .enter()
+		  .append('div')
+			 .attr('class', 'box');
+
+		// Add content
+		box
+					.append('div')
+						.attr('class', 'image')
+						.attr('style', function(d) {
+							if(d.Cover.length){
+								return 'background-image: url(' + d.Cover + ')';
+							}
+							else {
+								return 'background-image: url(/)';
+							}
+						});
+		box
+			.append('h4').append('a')
+				.attr('href', function (d) {
+					return '/shelter/' + d.shelter_id;
+				})
+				.text(function (d) { 
+					return d.Attributes.nameofshelter; 
+				});
+		box
+			.append('p')
+				.attr('class', 'country')
+				.text(function (d) { 
+					return d.Attributes.country; 
+				});
+		 
+	}
+	
 	/**
 	 * EVENTS
 	 */
@@ -53,7 +94,7 @@ if("/" === loc) {
 	d3.json('/api/v0.2/shelters/latest/3', function (error, data) {
 			
 		// add spinner
-	    $('#newest').spin();
+	    $('#newestcontent').spin();
 		
 		// convert the api data into a simpler array
 		var d = Object.keys(data).map(function(k) { 
@@ -63,57 +104,12 @@ if("/" === loc) {
 											return m;
 										}
 									  });
+			
+		// generate the 3 blocks
+		generate('#newest', d);
 		
-		// function to generate blocls
-		var generate = function generate(flexbox, data) {
-
-			var fb = d3.select(flexbox);
-			
-
-			// create a box for each object in the data
-			var box = fb.selectAll('div')
-			  .data(data)
-			  .enter()
-			  .append('div')
-				 .attr('class', function (r) {
-						return 'box';
-					})
-
-			// Add content
-			box
-						.append('div')
-							.attr('class', 'image')
-							.attr('style', function(d) {
-								if(d.Cover.length){
-									return 'background-image: url(' + d.Cover + ')';
-								}
-								else {
-									return 'background-image: url(/)';
-								}
-							});
-			box
-				.append('h4').append('a')
-					.attr('href', function (d) {
-						return '/shelter/' + d.shelter_id;
-					})
-					.text(function (d) { 
-						return d.Attributes.nameofshelter; 
-					});
-			box
-				.append('p')
-					.attr('class', 'country')
-					.text(function (d) { 
-						return d.Attributes.country; 
-					});
-			 
-			}
-			
-			// generate the 3 blocks
-			generate('#newest', d);
-			
-			// stop spinner
-			$('#newest').spin(false);
-		
+		// stop spinner
+		$('#newestcontent').spin(false);
 	});
 	
 	
