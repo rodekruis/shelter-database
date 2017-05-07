dac0a28622b092d813aa9173a1e24a8b4226840c
= <mvanderveen@redcross.nl>
2017-05-07 16:16:30 +0200

    fix issues #23 #19

--- src/web/static/js/edit-shelter.js
+++ src/web/static/js/edit-shelter.js
@@ -174,7 +174,7 @@ function update_property(property_id, id_of_values) {
 			  uploadMultiple: true,
 			  parallelUploads: 1,
 			  maxFilesize: 4, // MB
-			  acceptedFiles: "application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/docx, application/pdf, text/plain, application/msword, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+			  acceptedFiles: "application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/docx, application/pdf, text/plain, application/msword, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/acad,application/x-acad,application/autocad_dwg,image/x-dwg,application/dwg,application/x-dwg,application/x-autocad,image/vnd.dwg,drawing/dwg,application/vnd.sketchup.skp",
 			  addRemoveLinks: true,
 			  init: function() {
 				this.on("success", function(file, responseText) {
