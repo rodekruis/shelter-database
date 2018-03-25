0b377b43f87e08a075e7a83e941ee2f692110bf2
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-03-25 23:44:05 +0200

    Remoed now useless bowerrc file. Added weasyprint in Pipfile.

--- datadump_shelters.sh
+++ datadump_shelters.sh
@@ -1,2 +1,2 @@
-python3.5 src/manager.py export_shelters data/shelters/ overwrite
-python3.5 src/manager.py export_shelters_pictures data/shelters/ shelterdump_pictures
+python src/manager.py export_shelters data/shelters/ overwrite
+python src/manager.py export_shelters_pictures data/shelters/ shelterdump_pictures
